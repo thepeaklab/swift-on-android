2374505600bd3bae886e189ade89ff680a062bcb
Sören Gade <gade.soeren@gmail.com>
2019-12-31 14:06:14 +0100

    Add note about @_cdecl.

--- AndroidSampleLib/Sources/AndroidSampleLib/AndroidSampleLib.swift
+++ AndroidSampleLib/Sources/AndroidSampleLib/AndroidSampleLib.swift
@@ -1,3 +1,6 @@
+// According to this thread (https://news.ycombinator.com/item?id=13487339),
+// @_silgen_name only changes the link name and not the calling convention.
+// @_cdecl might help here.
 @_silgen_name("Java_com_example_androidcpp_HelloWorldJNI_returnIntegerSwift")
 public func returnIntegerSwift() -> Int {
     return 10000
